e7a7f8367a16ed32e8c0801999bf07b75ded344b
MyKe01 <mstasi01@gmail.com>
2023-03-14 14:51:41 +0100

    board entity

--- src/main/java/model/entities/Board.java
+++ src/main/java/model/entities/Board.java
@@ -1,13 +1,45 @@
 package model.entities;
 
 public class Board {
-    Box[][] board;
+    private Box[][] board;
+    private EndGameToken token;
     public Board(){
+        board = new Box[9][9];
+        for(int i =0; i<9; i++){
+            for (int j =0 ; j < 9; j++){
+                if (    (i == 0 && ( j<=2 || j>=5 )) ||
+                        (j == 0 && ( i<=2 || i>=5 )) ||
+                        (i == 8 && ( j<=2 || j>=5 )) ||
+                        (j == 8 && ( i<=2 || i>=5 )) ||
+                        (i == 1 && ( j <= 2 || j>=6 )) ||
+                        (j == 1 && ( i <= 2 || i>=6 )) ||
+                        (i == 7 && ( j <= 2 || j>=6 )) ||
+                        (j == 7 && ( i <= 2 || i>=6 ))
+                ) {
+                    board[i][j] = new Box(false, null);
+                }
+            }
+        }
         //new Box(true, null);
+        token = new EndGameToken(1);
     }
 
-    public createBoard(){
-
+    public Box getBox(int i, int j) throws IndexOutOfBoundsException{
+        if(!board[i][j].getValid()){
+            //todo gestione eccezione
+            throw new IndexOutOfBoundsException();
+        }else{
+            return board[i][j];
+        }
     }
 
+    public ItemTiles draw(int i, int j) {
+        ItemTiles res = getBox(i, j).getItemContained();
+        getBox(i,j).setContent(null);
+        return res;
+    }
+
+    public void setToken(EndGameToken token) {
+        this.token = token;
+    }
 }
