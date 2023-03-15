803480ff52e3e64f4f1739b31a26b3b78033fbea
nicolotocalli <nicolo.tocalli@gmail.com>
2023-03-15 17:42:46 +0100

    attributes lenght height for Bookshelf

--- src/main/java/model/Bookshelf.java
+++ src/main/java/model/Bookshelf.java
@@ -2,6 +2,8 @@ package model;
 
 public class Bookshelf {
     private Boolean full;
+
+    private int length , height;
     private int[] actualColumnLength;
     private ItemTiles[];
 
--- src/main/java/model/goals/CheckEightEquals.java
+++ src/main/java/model/goals/CheckEightEquals.java
@@ -1,4 +1,17 @@
 package model.goals;
 
+import model.Bookshelf;
+import model.ScoringToken;
+
 public class CheckEightEquals extends CommonGoal{
+
+    public CheckEightEquals(){
+        super();
+    }
+
+    @Override
+    public ScoringToken validate(Bookshelf bookshelf) {
+        int equals , i , j;
+        return null;
+    }
 }
