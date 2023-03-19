08925693f4e40ebe504de79ce5a2451ea88d5099
MyKe01 <mstasi01@gmail.com>
2023-03-19 15:13:06 +0100

    modified pom.xml

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,46 @@
 package model;
 
+import model.ItemTiles;
+import model.TablePosition;
+
 public class Player {
+    private String username;
+    private boolean status;
+    private int score;
+    private int currentDrowable;
+
+    private TablePosition currentPosition;
+
+
+    public Player(TablePosition currentPosition,String username){
+
+        this.currentPosition=currentPosition;
+        this.username=username;
+
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getCurrentDrowable() {
+        return currentDrowable;
+    }
+
+    public void setStatus(boolean status){
+        this.status=status;
+    }
+
+    public void insert(int column, ItemTiles card) throws Exception {
+        currentPosition.getBookshelf().insert(card);
+    }
+
 }
