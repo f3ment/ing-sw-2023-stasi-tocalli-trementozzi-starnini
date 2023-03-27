e8809eb166eb9f8946fdf45441855517e2141f93
nicolotocalli <nicolo.tocalli@gmail.com>
2023-03-27 20:09:37 +0200

    CheckCrossTest implemented but not completed (todo: add more test cases)

--- src/main/java/model/goals/CheckCross.java
+++ src/main/java/model/goals/CheckCross.java
@@ -11,6 +11,10 @@ public class CheckCross extends CommonGoal{
 
     public CheckCross(int romanNumber, int numberPlayers){
         super(romanNumber, numberPlayers);
+
+        this.completed = false;
+        this.romanNumber = romanNumber;
+        this.stack = new Stack(numberPlayers , this);
     }
 
 
@@ -21,12 +25,19 @@ public class CheckCross extends CommonGoal{
             for (int i = 0; i < bookshelf.getHeight(); i++) {
                 for (int j = 0; j < bookshelf.getLength(); j++) {
                     if (i < bookshelf.getHeight() - 2 && j < bookshelf.getLength() - 2) {
-                        if (bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i, j + 2).getType()) &&
-                                bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 1, j + 1).getType()) &&
-                                bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 2, j).getType()) &&
-                                bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 2, j + 2).getType())) {
-                            return stack.pop();  // Esiste una <X> dello stesso tipo
+                        try {
+                            if (bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i, j + 2).getType()) &&
+                                    bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 1, j + 1).getType()) &&
+                                    bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 2, j).getType()) &&
+                                    bookshelf.getItem(i, j).getType().equals(bookshelf.getItem(i + 2, j + 2).getType())) {
+                                return stack.pop();  // Esiste una <X> dello stesso tipo
+                            }
+                        }catch (Exception e){
+                            continue;
                         }
+
+
+
                     } else continue;
                 }
             }
