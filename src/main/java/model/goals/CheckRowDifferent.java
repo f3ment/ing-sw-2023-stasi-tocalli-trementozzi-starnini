f3f9878e6f11729f0bcc6105dc5287cdcbb9d292
sterning17 <sterning17@gmail.com>
2023-03-28 12:28:58 +0200

    correction

--- src/main/java/model/goals/CheckRowDifferent.java
+++ src/main/java/model/goals/CheckRowDifferent.java
@@ -38,7 +38,7 @@ public class CheckRowDifferent extends CommonGoal{
                 }
             }
 
-            if(counterDiffTypes <= 3 && strategy ){
+            if(counterDiffTypes <= 3&& counterDiffTypes > 0 && strategy ){
                 rep ++;
             }else if(counterDiffTypes == 5 && !strategy){
                 rep ++;
