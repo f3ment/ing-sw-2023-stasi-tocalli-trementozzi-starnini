af1ec0ec7162ff560fa4c5a28cb005f927b1924d
f3ment <f3ment@protonmail.com>
2023-03-29 20:21:53 +0200

    BagTest init

--- src/test/java/model/BagTest.java
+++ src/test/java/model/BagTest.java
@@ -2,19 +2,53 @@ package model;
 
 import model.board.Board;
 import model.board.FourBoard;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagTest {
 
+
+    @Test
+    void fillEmptyTest() {
+        try {
+
+            Bag bag = new Bag();
+
+            assertTrue(bag.getLeftItems() == 132);
+
+            while (bag.getLeftItems() > 0) {
+                bag.extract();
+            }
+            assertTrue(bag.getLeftItems() == 0);
+
+
+
+        }catch (Exception e){
+            System.out.println("<fillEmptyTest> Non Riuscito!");
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
+
     @Test
     void extract() {
-        Bag bag = new Bag();
-        assertTrue(bag.getLeftItems() == 132);
-        while(bag.getLeftItems()>0){
-            bag.extract();
-        }
+            try {
 
-    }
+                Bag bag = new Bag();
+
+                assertTrue(bag.getLeftItems() == 132);
+
+
+
+
+            } catch (Exception e) {
+                System.out.println("<extract> Non Riuscito!");
+                System.out.println(e.getMessage());
+                System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
 }
