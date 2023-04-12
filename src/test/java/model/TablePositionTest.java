d723d5ee16100a9c580229d41a0a9221a1324c70
sterning17 <sterning17@gmail.com>
2023-04-12 23:55:57 +0200

    tablePositionTest

--- src/test/java/model/TablePositionTest.java
+++ src/test/java/model/TablePositionTest.java
@@ -74,6 +74,51 @@ class TablePositionTest {
             bookshelf.setChoosenColumn(4);
             bookshelf.insert(new ItemTiles(Type.CATS, 1));
             bookshelf.insert(new ItemTiles(Type.CATS, 1));
+
+            TablePosition table = new TablePosition(new String("Michi"), new PersonalGoal(null), bookshelf);
+
+            Bookshelf book2 = new Bookshelf();
+            book2.setChoosenColumn(0);
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.FRAMES, 1));
+            book2.insert(new ItemTiles(Type.BOOKS, 1));
+            book2.insert(new ItemTiles(Type.PLANTS, 1));
+
+            book2.setChoosenColumn(1);
+
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.GAMES, 1));
+            book2.insert(new ItemTiles(Type.BOOKS, 1));
+            book2.insert(new ItemTiles(Type.PLANTS, 1));
+            book2.insert(new ItemTiles(Type.PLANTS, 1));
+
+            book2.setChoosenColumn(2);
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.CATS, 1));
+            book2.insert(new ItemTiles(Type.TROPHIES, 1));
+            book2.insert(new ItemTiles(Type.FRAMES, 1));
+            book2.insert(new ItemTiles(Type.PLANTS, 1));
+            book2.insert(new ItemTiles(Type.PLANTS, 1));
+
+            book2.setChoosenColumn(3);
+            book2.insert(new ItemTiles(Type.CATS, 1));
+            book2.insert(new ItemTiles(Type.CATS, 1));
+            book2.insert(new ItemTiles(Type.GAMES, 1));
+            book2.insert(new ItemTiles(Type.BOOKS, 1));
+            book2.insert(new ItemTiles(Type.CATS, 1));
+
+            book2.setChoosenColumn(4);
+            book2.insert(new ItemTiles(Type.CATS, 1));
+            book2.insert(new ItemTiles(Type.CATS, 1));
+
+
+            assertTrue(table.getBookshelf().getItem(1,1).getType().equals(book2.getItem(1,1).getType()));
+            assertTrue(table.getBookshelf().getItem(3,1).getType().equals(book2.getItem(3,1).getType()));
+            assertTrue(table.getBookshelf().getItem(5,2).getType().equals(book2.getItem(5,2).getType()));
+
         }catch(Exception e){
             System.out.println("Test fallito!");
             System.out.println(e.getMessage());
@@ -82,10 +127,13 @@ class TablePositionTest {
     }
 
     @Test
-    void setFirstPosition() {
+    void setFirstPosition() throws IOException { //and isfirstposition
+        TablePosition table = new TablePosition(new String("Michi"), new PersonalGoal(null), new Bookshelf());
+        table.setFirstPosition(false);
+        assertTrue(table.isFirstPosition()==false);
+        table.setFirstPosition(true);
+        assertTrue(table.isFirstPosition()==true);
     }
 
-    @Test
-    void isFirstPosition() {
-    }
+
 }
