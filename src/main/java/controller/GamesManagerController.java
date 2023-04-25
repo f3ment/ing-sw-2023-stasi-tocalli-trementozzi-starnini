2e5d4078806e36c5391383b10bc7399bdbbe640e
sterning17 <sterning17@gmail.com>
2023-04-25 23:13:02 +0200

    GamesManagerController

--- src/main/java/controller/GamesManagerController.java
+++ src/main/java/controller/GamesManagerController.java
@@ -1,13 +1,20 @@
 package controller;
 import model.LobbyManager.Lobby;
 import java.util.*;
+import java.io.*;
 
 
 import distributed.Client;
 import utils.Event;
-
+//TODO sincronizzare e gestire inizio game(magari farlo tornare)
 public class GamesManagerController {
-    public void update(Client client, Event event, Integer columnNumber, Object o, String userName) {
+    public void addPlayerToLobby(Client client, Integer playerNumber, String userName) {
+        for(Lobby a : lobbies){
+            if(a.isFull()==false&&a.getnPlayers()==playerNumber&&a.insertPlayer(client,userName)){
+                return;
+            }
+        }
+        lobbies.add(new Lobby(playerNumber,userName,client));
 
         /*
         *
