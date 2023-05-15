e98cd33349bdf4c3fa9db7c4400de1ad45394aea
nicolotocalli <nicolo.tocalli@gmail.com>
2023-05-15 19:03:42 +0200

    javafx dependencies added in pom.xml gui start and login pages early implementation -to start the gui run "GuiLauncher.java"

---
+++ src/main/java/view/GuiLauncher.java
@@ -0,0 +1,8 @@
+package view;
+
+public class GuiLauncher {
+    public static void main(String[] args) {
+        HelloApplication MainGui = new HelloApplication();
+        MainGui.main(args);
+    }
+}
---
+++ src/main/java/view/HelloApplication.java
@@ -0,0 +1,24 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("MyShelfie");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
