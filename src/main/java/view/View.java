69c785e58891d99669097ec694c7ca77422543c4
nicolotocalli <nicolo.tocalli@gmail.com>
2023-05-19 17:16:25 +0200

    addition to last commit

---
+++ src/main/java/view/View.java
@@ -0,0 +1,15 @@
+package view;
+
+
+import model.Message;
+import utils.Event;
+import utils.Observable;
+
+public abstract class View extends Observable<Event>{
+
+    public abstract void update(Message message);
+
+    public abstract void run();
+
+
+}
